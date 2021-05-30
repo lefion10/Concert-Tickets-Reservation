9b56fd75d58adaa4496a3fffbee27276d615ab27
Puscuta Alexandru-Ionut <74986114+lefion10@users.noreply.github.com>
2021-05-30 16:58:55 +0300

    Test run

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        //TEST RUN
+        TkCount tkcount = new TkCount(50,50);
+
+        TkReservation test1 = new TkReservation(tkcount,"James","Smith",10,1);
+        TkReservation test2 = new TkReservation(tkcount,"Robert","Smith",50,1);
+
+        test1.start();
+        test2.start();
     }
 }
